73d16e776480618313dd17ab9e5005f2688e99e6
Tomide <akintomideayomide88@gmail.com>
2025-07-03 14:45:32 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import BenefitContainer from './views/benefitContainer'
-import BigPicture from './views/BigPicture'
-import EfficencyHero from './views/EfficiencyHero'
-import Navbar from './views/Navbar'
-import TrustedBy from './views/TrustedBy'
-import MapSuccess from './views/MapSuccess'
-import WhyChooseArea from './views/WhyChooseArea'
-import TestmonialCard from './views/TestimonialCard'
-import Navbarmock from './views/navbarmock'
-import Footer from './views/Footer'
-import ConnectWithUs from './views/ConnectWithUs'
+import BenefitContainer from "./views/benefitContainer";
+import BigPicture from "./views/BigPicture";
+import EfficencyHero from "./views/EfficiencyHero";
+import Navbar from "./views/Navbar";
+import TrustedBy from "./views/TrustedBy";
+import MapSuccess from "./views/MapSuccess";
+import WhyChooseArea from "./views/WhyChooseArea";
+import TestmonialCard from "./views/TestimonialCard";
+import Navbarmock from "./views/navbarmock";
+import Footer from "./views/Footer";
+import ConnectWithUs from "./views/ConnectWithUs";
 function App() {
-
-
   return (
     <>
       <Navbar />
-      <Navbarmock/>
+      <Navbarmock />
       <EfficencyHero />
       <TrustedBy />
       <BenefitContainer />
       <BigPicture />
-      <WhyChooseArea/>
-      <TestmonialCard/>
-      <MapSuccess/>
-      <ConnectWithUs/>
-      <Footer/>
+      <WhyChooseArea />
+      <TestmonialCard />
+      <MapSuccess />
+      <ConnectWithUs />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
